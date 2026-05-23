eba49a5a6e652cf6f749aa86a566f5dac2492a3b
Ganyi Mo <gmoo0012@student.monash.com>
2026-05-23 20:37:14 +1000

    update

--- src/main/java/com/hmdp/controller/BlogImageController.java
+++ src/main/java/com/hmdp/controller/BlogImageController.java
@@ -1,12 +1,12 @@
-// 文件说明：UploadController 控制器，负责处理 Upload 相关的 HTTP 接口请求。
+// 文件说明：BlogImageController 控制器，负责处理博客图片上传和删除。
 
 package com.hmdp.controller;
 
 import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.StrUtil;
 import com.hmdp.dto.Result;
-import com.hmdp.utils.SystemConstants;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,21 +20,25 @@ import java.util.UUID;
 // 公共路径前缀：upload
 @RequestMapping("upload")
 // 控制器类：负责接收请求、调用业务层并返回结果
-public class UploadController {
+public class BlogImageController {
+
+    // 博客图片上传目录，通过 application.yaml 配置，避免把本机路径写死在代码里
+    @Value("${hmdp.upload.image-dir}")
+    private String imageUploadDir;
 
     // 上传博客图片
     @PostMapping("blog")
-    public Result uploadImage(@RequestParam("file") MultipartFile image) {
+    public Result uploadBlogImage(@RequestParam("file") MultipartFile file) {
         try {
             // 获取原始文件名称
-            String originalFilename = image.getOriginalFilename();
+            String originalFilename = file.getOriginalFilename();
             // 生成新文件名
-            String fileName = createNewFileName(originalFilename);
+            String relativePath = createBlogImagePath(originalFilename);
             // 保存文件
-            image.transferTo(new File(SystemConstants.IMAGE_UPLOAD_DIR, fileName));
+            file.transferTo(new File(imageUploadDir, relativePath));
             // 返回结果
-            log.debug("文件上传成功，{}", fileName);
-            return Result.ok(fileName);
+            log.debug("文件上传成功，{}", relativePath);
+            return Result.ok("/" + relativePath);
         } catch (IOException e) {
             throw new RuntimeException("文件上传失败", e);
         }
@@ -42,31 +46,32 @@ public class UploadController {
 
     // 删除博客图片
     @GetMapping("/blog/delete")
-    public Result deleteBlogImg(@RequestParam("name") String filename) {
-        File file = new File(SystemConstants.IMAGE_UPLOAD_DIR, filename);
-        if (file.isDirectory()) {
+    public Result deleteBlogImage(@RequestParam("name") String imagePath) {
+        String relativePath = StrUtil.removePrefix(imagePath, "/");
+        File imageFile = new File(imageUploadDir, relativePath);
+        if (imageFile.isDirectory()) {
             return Result.fail("错误的文件名称");
         }
-        FileUtil.del(file);
+        FileUtil.del(imageFile);
         return Result.ok();
     }
 
     // 生成分目录的新文件名
-    private String createNewFileName(String originalFilename) {
+    private String createBlogImagePath(String originalFilename) {
         // 获取后缀
         String suffix = StrUtil.subAfter(originalFilename, ".", true);
         // 生成目录
-        String name = UUID.randomUUID().toString();
-        int hash = name.hashCode();
+        String imageName = UUID.randomUUID().toString();
+        int hash = imageName.hashCode();
         int d1 = hash & 0xF;
         int d2 = (hash >> 4) & 0xF;
         // 判断目录是否存在
-        File dir = new File(SystemConstants.IMAGE_UPLOAD_DIR, StrUtil.format("/blogs/{}/{}", d1, d2));
+        File dir = new File(imageUploadDir, StrUtil.format("blogs/{}/{}", d1, d2));
         if (!dir.exists()) {
             dir.mkdirs();
         }
         // 生成文件名
-        return StrUtil.format("/blogs/{}/{}/{}.{}", d1, d2, name, suffix);
+        return StrUtil.format("blogs/{}/{}/{}.{}", d1, d2, imageName, suffix);
     }
 
 }
